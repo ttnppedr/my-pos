3289166adc78a19b0227d8f5ff39e3f91e97a582
ttnppedr <priest0204@gmail.com>
2022-02-22 21:27:03 +0800

    auth in cashier page and redirect wildcard to login

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('/', \App\Http\Livewire\Home::class)->name('home');
 Route::get('/products', \App\Http\Livewire\Products::class)->name('products');
 Route::get('/cart', \App\Http\Livewire\Cart::class)->name('cart');
 
-Route::get('/cashier',\App\Http\Livewire\Cashier::class)->name('cashier');
+Route::middleware(['auth:sanctum', 'verified'])->get('/cashier', \App\Http\Livewire\Cashier::class)->name('cashier');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/{any}', function () {
+    return redirect()->route('login');
+})->where('any', '.*');
