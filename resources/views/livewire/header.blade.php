b7e49642e76025f8493db9e134d5e6aa95e42b70
ttnppedr <priest0204@gmail.com>
2022-03-02 19:40:15 +0800

    add header

---
+++ resources/views/livewire/header.blade.php
@@ -0,0 +1,34 @@
+<div>
+    <div class="shadow-[0_1px_4px_1px_#e5e5e5] h-[80px] flex justify-between py-[15px] px-[25px] ">
+        <h1 class="text-3xl my-[4px]">訂單列表</h1>
+        <nav>
+            <ul class="flex text-2xl">
+                <li>
+                    <button class="bg-[#0f375b] text-white h-[50px] w-[80px] rounded mr-4">訂單</button>
+                </li>
+                <li>
+                    <button class="bg-[#0f375b] text-white h-[50px] w-[80px] rounded mr-4">新增</button>
+                </li>
+                <li>
+                    <button
+                        class="bg-[#0f375b] text-white h-[50px] w-[50px] rounded mr-4 flex justify-center items-center">
+                        <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M16.763 16.763a1.364 1.364 0 0 1 1.929 0l8.58 8.581v-4.89a1.364 1.364 0 0 1 2.728 0v8.182c0 .753-.61 1.363-1.364 1.363L20.455 30a1.364 1.364 0 0 1 0-2.727h4.89l-8.582-8.582a1.364 1.364 0 0 1 0-1.928zm-5.454 0a1.364 1.364 0 1 1 1.928 1.929l-8.581 8.58h4.89a1.364 1.364 0 0 1 0 2.728H1.363c-.753 0-1.363-.61-1.363-1.364L0 20.455a1.364 1.364 0 0 1 2.727 0v4.89zM9.545 0a1.364 1.364 0 0 1 0 2.727h-4.89l8.582 8.582a1.364 1.364 0 1 1-1.929 1.928l-8.58-8.581v4.89a1.364 1.364 0 0 1-2.728 0V1.363C0 .611.61.001 1.364.001zm19.09 0C29.39 0 30 .61 30 1.364L30 9.545a1.364 1.364 0 0 1-2.727 0v-4.89l-8.582 8.582a1.36 1.36 0 0 1-1.928 0 1.364 1.364 0 0 1 0-1.929l8.581-8.58h-4.89a1.364 1.364 0 0 1 0-2.728z"
+                                fill="#FFF" fill-rule="nonzero"/>
+                        </svg>
+                    </button>
+                </li>
+                <li>
+                    <button class="bg-[#0f375b] text-white h-[50px] w-[50px] rounded flex justify-center items-center">
+                        <svg width="30" height="30" viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M3 21v3a6 6 0 0 0 6 6h15c3.315 0 6-2.685 6-6V6a6 6 0 0 0-6-6H9a6 6 0 0 0-6 6v3a1.5 1.5 0 0 0 3 0V6c0-1.656 1.344-3 3-3h15a3 3 0 0 1 3 3v18a3 3 0 0 1-3 3H9a3 3 0 0 1-3-3v-3c0-.826-.672-1.5-1.5-1.5S3 20.174 3 21zm11.379-7.5-1.94-1.938a1.501 1.501 0 0 1 2.121-2.123l4.5 4.5a1.5 1.5 0 0 1 0 2.123l-4.5 4.5a1.501 1.501 0 0 1-2.12-2.123l1.939-1.939H1.5a1.5 1.5 0 0 1 0-3h12.879z"
+                                fill="#FFF" fill-rule="evenodd"/>
+                        </svg>
+                    </button>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
