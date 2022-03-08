8a15f25e2a7c50a745484cb4f38d8ebb1518b233
ttnppedr <priest0204@gmail.com>
2022-03-08 20:11:46 +0800

    add product into cart

--- app/Http/Livewire/NewOrder.php
+++ app/Http/Livewire/NewOrder.php
@@ -7,10 +7,41 @@ use Livewire\Component;
 
 class NewOrder extends Component
 {
+    public $cart = [];
+
     public function render()
     {
         return view('livewire.new-order', [
             'products' => Product::all(),
+            'cart' => $this->cart,
+            'amountReceivable' => $this->calculateAmountReceivable(),
         ]);
+    }
+
+    public function calculateAmountReceivable()
+    {
+        return array_reduce($this->cart, function ($sum, $product) {
+            $sum += $product['price'] * $product['quantity'];
+            return $sum;
+        }, 0);
+    }
+
+    public function addToCartFromProductList($productId)
+    {
+        $cart = collect($this->cart);
+        $product = Product::select(['id', 'name', 'price'])->find($productId)->toArray();
+
+        if ($cart->where('name', $product['name'])->where('price', $product['price'])->count() > 0) {
+            $this->cart = $cart->map(function ($item) use ($product) {
+                if ($item['name'] === $product['name'] && $item['price'] === $product['price']) {
+                    $item['id'] = $product['id'];
+                    $item['quantity']++;
+                }
+                return $item;
+            })->toArray();
+            return;
+        }
+
+        $this->cart[] = $product + ['quantity' => 1];
     }
 }
