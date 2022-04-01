78adf3f374884e852d227190c4edff0713c10aa5
ttnppedr <priest0204@gmail.com>
2022-04-01 14:46:54 +0800

    show amount_received when order completed in orders page

--- resources/views/livewire/orders.blade.php
+++ resources/views/livewire/orders.blade.php
@@ -9,13 +9,19 @@
                     <div class="h-full bg-white p-4 font-bold border border-[#e5e5e5] rounded cursor-pointer"
                     >
                         <div class="flex justify-between mb-4 text-2xl text-[#0f375b]">
-                            <div>
-                                @if ($order->status !== 2)
+                            @if ($order->status !== 2)
+                                <div>
                                     <span class="text-red-700">*</span>
-                                @endif
-                                <span>{{$order->note}}</span>
-                            </div>
-                            <span>${{number_format($order->amount_receivable, 0, '', ',')}}</span>
+                                    <span>{{$order->note}}</span>
+                                </div>
+                                <span>${{number_format($order->amount_receivable, 0, '', ',')}}</span>
+                            @else
+                                <div>
+                                    <span>{{$order->note}}</span>
+                                </div>
+                                <span>${{number_format($order->amount_received, 0, '', ',')}}</span>
+
+                            @endif
                         </div>
                         <div class="text-[#2678c6] text-xl text-left">
                             {{$order->detail}}
